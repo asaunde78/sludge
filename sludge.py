063683d009117a5b3eee5b3464f68f4e90cbbb2f
asaunde78 <saundersasher78@gmail.com>
2024-04-22 15:46:57 -0400

    initial commit

---
+++ sludge.py
@@ -0,0 +1,91 @@
+
+import wave
+import json
+from yt_dlp import YoutubeDL, utils
+import random 
+import os
+import subprocess
+
+
+
+from vosk import Model, KaldiRecognizer, SetLogLevel
+
+
+
+mc_url = "https://www.youtube.com/watch?v=n_Dv4JMiwK8"
+
+def filename_hook(d):
+    # print(d)
+    if d["status"] == "finished":
+        # print(d)
+
+        os.rename(d["filename"], f"videos/outfile.mp4")
+
+
+    
+
+
+model_path = "vosk-model-en-us-0.22-lgraph"
+
+audio_file = "test.wav"
+
+model = Model(model_path)
+
+wf = wave.open(audio_file, "rb")
+rec = KaldiRecognizer(model,wf.getframerate())
+rec.SetWords(True)
+
+results = []
+while True:
+    data = wf.readframes(4000)
+    if(len(data)==0):
+        break
+    if rec.AcceptWaveform(data):
+        part_result = json.loads(rec.Result())
+        results.append(part_result)
+part_result =  json.loads(rec.FinalResult())
+results.append(part_result)
+# print(results)
+
+
+with open("subtitles.srt", "w") as f:
+    f.write("")
+with open("subtitles.srt", "a") as subt:
+    print(results)
+
+    for i,word in enumerate(results[0]["result"]):
+        print("WORD: ", word)
+        start = word["start"]
+        end = word["end"]
+        txt = word["word"]
+        stri = f"{i+1}\n00:00:0{start} --> 00:00:0{end}\n\n{txt}\n\n"
+        print(stri)
+        subt.write(stri)
+
+
+
+opts = {
+    "default_search":"ytsearch",
+}
+
+with YoutubeDL(opts) as infograb:
+    info = infograb.extract_info(mc_url, download=False)
+    print(info["duration"])
+    end = results[0]["result"][-1]["end"]
+    start = random.uniform(0, info["duration"]- end)
+    
+    ydl_opts = {
+        "paths": {"home": "videos"},
+        "format": "mp4",
+        "concurrent_fragment_downloads":5,
+        "progress_hooks": [filename_hook],
+        "default_search":"ytsearch",
+        "download_ranges":utils.download_range_func(None, [(start, end+start)]),  
+        "force_keyframes_at_cuts": True, 
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download(mc_url)
+        
+        ffmpeg_cmd = ['ffmpeg', '-i','videos/outfile.mp4', '-vf', 'subtitles=subtitles.srt', 'videos/outfilesubtitled.mp4']
+        subprocess.run(ffmpeg_cmd)
+            
